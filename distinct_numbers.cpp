1550db9a078c3e1fd76e58b1ed9f8edf96268180
SSJNM <nishant010singh@gmail.com>
2024-03-02 15:14:05 +0530

    Adding Labyrinth Solution

---
+++ distinct_numbers.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+#define ln 
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+int main(){
+    fast_cin();
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    int num=-1;
+    int count=0;
+    for(auto n:v){
+        if(num != n){
+            count++;
+            num=n;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
--- labyrinth.cpp
+++ labyrinth.cpp
@@ -4,59 +4,34 @@ using namespace std;
 
 pair<int,int> A;
 pair<int,int> B;
-int finaldist=0;
-string finalpath;
+
 
 vector<pair<int,int>> dir={{0,-1},{0,1},{1,0},{-1,0}};
 
 bool check(vector<string>& grid,int x,int y){
     return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
 }
-void bfs(vector<string>& grid,vector<vector<pair<int,int>>>& grid_path,queue<pair<int,int>>& q,int dist){
+
+void bfs(vector<string>& grid,vector<vector<pair<int,int>>>& grid_path,queue<pair<int,int>>& q,int& dist){
     while(!q.empty()){
-        int x=q.front().first;
-        int y=q.front().second;
-        if(x == B.first && y == B.second) break;
-        grid[x][y] = '#';
-        dist++;
         int sz = q.size();
         for(int i=0;i<sz;i++){
+            int x=q.front().first;
+            int y=q.front().second;
+            if(x == B.first && y == B.second) return;
+            grid[x][y] = '#';
+            q.pop();
             for(auto d:dir){
                 if(check(grid,x+d.first,y+d.second) && grid[x+d.first][y+d.second] != '#'){
                     q.push({x+d.first,y+d.second});
-                    grid_path[x+d.first][y+d.second] = {x,y};
+                    grid_path[x+d.first][y+d.second].first = x;
+                    grid_path[x+d.first][y+d.second].second = y;
+                    grid[x+d.first][y+d.second] = '#';
                 }
             }
-            q.pop();
-            x=q.front().first;
-            y=q.front().second;
-            if(x == B.first && y == B.second) break;
         }
+        dist++;
     }
-    if(q.empty()){
-        cout<<"NO"<<endl;
-    }
-    cout<<"YES"<<endl;
-    cout<<dist<<endl;
-    string path;
-    int x = B.first;
-    int y = B.second;
-    while(!(x == -1 && y == -1)){
-        int px = grid_path[x][y].first;
-        int py = grid_path[x][y].second;
-        if(x == px+1) 
-        path.push_back('D');
-        else if(x == px-1) 
-        path.push_back('U');
-        else if(y == py+1)
-        path.push_back('R');
-        else if(y == py-1) 
-        path.push_back('L');
-        x = px;
-        y = py;
-    }
-    reverse(path.begin(),path.end());
-    cout<<path<<endl;
 }
 
 int main(){
@@ -81,5 +56,39 @@ int main(){
     queue<pair<int,int>> q;
     q.push(A);
     bfs(grid,grid_path,q,dist);
+    // for(auto x:grid_path){
+    //     for(auto y:x){
+    //         cout<<"{"<<y.first<<","<<y.second<<"} ";
+    //     }
+    //     cout<<endl;
+    // }
+    grid_path[A.first][A.second] = {A.first,A.second};
+    if(q.empty()){
+        cout<<"NO"<<endl;
+        return 0;
+    }
+    cout<<"YES"<<endl;
+    string path;
+    int x = B.first;
+    int y = B.second;
+    int px = grid_path[x][y].first;
+    int py = grid_path[x][y].second;
+    while(!(x == px && y == py)){
+        if(x == px+1) 
+        path.push_back('D');
+        else if(x == px-1) 
+        path.push_back('U');
+        else if(y == py+1)
+        path.push_back('R');
+        else if(y == py-1) 
+        path.push_back('L');
+        x = px;
+        y = py;
+        px = grid_path[x][y].first;
+        py = grid_path[x][y].second;
+    }
+    reverse(path.begin(),path.end());
+    cout<<dist<<endl;
+    cout<<path<<endl;
     return 0;
-}
+} 
